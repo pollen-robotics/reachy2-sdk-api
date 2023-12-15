829b2020ecfcd88760c84e10905823d7c7fefb2a
RemiFabre <remifabre1800@gmail.com>
2023-12-15 21:11:24 +0100

    compiling pb2.py

--- python/reachy2_sdk_api/arm_pb2_grpc.py
+++ python/reachy2_sdk_api/arm_pb2_grpc.py
@@ -97,6 +97,11 @@ class ArmServiceStub(object):
                 request_serializer=arm__pb2.SpeedLimitRequest.SerializeToString,
                 response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
                 )
+        self.SendArmCartesianGoal = channel.unary_unary(
+                '/reachy.part.arm.ArmService/SendArmCartesianGoal',
+                request_serializer=arm__pb2.ArmCartesianGoal.SerializeToString,
+                response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                )
 
 
 class ArmServiceServicer(object):
@@ -198,6 +203,12 @@ class ArmServiceServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def SendArmCartesianGoal(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_ArmServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -279,6 +290,11 @@ def add_ArmServiceServicer_to_server(servicer, server):
             'SetSpeedLimit': grpc.unary_unary_rpc_method_handler(
                     servicer.SetSpeedLimit,
                     request_deserializer=arm__pb2.SpeedLimitRequest.FromString,
+                    response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+            ),
+            'SendArmCartesianGoal': grpc.unary_unary_rpc_method_handler(
+                    servicer.SendArmCartesianGoal,
+                    request_deserializer=arm__pb2.ArmCartesianGoal.FromString,
                     response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
             ),
     }
@@ -559,6 +575,23 @@ class ArmService(object):
             metadata=None):
         return grpc.experimental.unary_unary(request, target, '/reachy.part.arm.ArmService/SetSpeedLimit',
             arm__pb2.SpeedLimitRequest.SerializeToString,
+            google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def SendArmCartesianGoal(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/reachy.part.arm.ArmService/SendArmCartesianGoal',
+            arm__pb2.ArmCartesianGoal.SerializeToString,
             google_dot_protobuf_dot_empty__pb2.Empty.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
