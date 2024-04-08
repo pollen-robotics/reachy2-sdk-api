5cff71cad6bbcf1742bf0391391c1b0e5aa1d73b
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-04-08 15:28:42 +0200

    change package version in setup.py

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name="reachy2-sdk-api",
-    version="0.1.0",
+    version="1.0.1",
     packages=find_packages(exclude=["tests"]),
     install_requires=[
         "grpcio>=1.59.0, <=1.62.2",
