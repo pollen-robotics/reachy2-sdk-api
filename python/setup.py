d4c27fe2557daa804531dce35ef87720d40dc7dc
Gaëlle Lannuzel <gaelle.lannuzel@pollen-robotics.com>
2025-09-04 18:11:45 +0200

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -24,7 +24,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     install_requires=[
         "grpcio>=1.59.0, <=1.73.1",
-        "grpcio-tools>=1.59.0, <=1.62.2",
+        "grpcio-tools>=1.59.0, <=1.73.1",
         "protobuf>=4.25.0, <=6.32.0",
     ],
     author="Pollen Robotics",
